26484a7830f383e624452bc691a36746788f7ff2
RyougikiMikiya <2467281535@qq.com>
2021-10-26 23:52:22 +0800

    关于临时变量mutex的一些注释

--- src/lib.rs
+++ src/lib.rs
@@ -159,12 +159,18 @@ pub mod thread_pool {
         fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
             let thread =  thread::spawn( move ||{
                 loop {
+                    // let 语句结束后，临时变量的lockguard就被丢掉了，所以也释放了锁，这才能让下一个线程可以继续收到消息再取走任务
                     let job = receiver.lock().unwrap().recv().unwrap();
                     job();
+                    // 显示的例子，这样guard的生命期覆盖了job，job中又有read阻塞住了,job不执行完那mutex就不会释放，其他线程也无法拿到receiver的job，就必须显示的drop掉guard来释放锁。
+                    // let guard = receiver.lock().unwrap();
+                    // let job = guard.recv().unwrap();
+                    // std::mem::drop(guard);
+                    // job();
                 }
+                // 更傻逼了，while let是表达式，临时变量gurad在整个表达式期间都有效所以没法释放
                 // while let Ok(job) = receiver.lock().unwrap().recv() {
                 //     println!("Worker {} got a job; executing.", id);
-    
                 //     job();
                 // }
             });
